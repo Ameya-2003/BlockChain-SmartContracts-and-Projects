1b04922f0b7d59fd8ca03b6b766ab25215941827
Ameya Muktewar <ameyamuktewargithub@gmail.com>
2023-12-05 20:51:28 +0530

    Boolean.sol
    Example of an Boolean data type in Solidity, using Remix IDE

---
+++ Solidiy/Boolean.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract ExampleBoolean {
+    bool public myBool;
+
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+}
+
+
